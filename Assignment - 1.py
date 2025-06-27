8bbda98be813921f6c52e1eadb7cedb701274e49
Bhola785 <164310904+Bhola785@users.noreply.github.com>
2025-06-27 11:25:06 +0530

    Add files via upload

---
+++ Assignment - 1.py
@@ -0,0 +1,27 @@
+''' Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen. '''
+
+'''a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+print("The sum of these a and b numbers is - ",a+b)
+print("The subtraction  of these a and b numbers is - ",a-b)
+print("The multiplication of these a and b numbers is - ",a*b)
+print("The division of these a and b numbers is - ",a/b)'''
+
+'''Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.'''
+
+a=input("Enter the first name: ")
+b=input("Enter the last name: ")
+c=a+ b
+print("Hello Mr.",c,"Welcome to Tutedude python programming course")
